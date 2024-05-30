a21520c2a699e2c9299c219e1213ab6be026d331
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-05-30 18:58:43 +0300

    Add AppHeader component

---
+++ components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="p-5 lg:px-12">
+    <nav>
+      <NuxtLink class="text-2xl hover:text-blue-500" to="/">Courses</NuxtLink>
+    </nav>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="wrapper">
-    <header class="p-5 lg:px-12">
-      <nav>
-        <NuxtLink class="text-2xl hover:text-blue-500" to="/">Courses</NuxtLink>
-      </nav>
-    </header>
+    <AppHeader />
     <slot></slot>
   </div>
 </template>
