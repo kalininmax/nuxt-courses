6d7efde1d17a7fe26cc7a58025c79b8f284166c8
Maksim Kalinin <kalinin.m.a@bk.ru>
2024-05-30 18:57:37 +0300

    Add default layout

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <header class="p-5 lg:px-12">
-      <nav>
-        <NuxtLink class="text-2xl hover:text-blue-500" to="/">Courses</NuxtLink>
-      </nav>
-    </header>
+  <NuxtLayout>
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
